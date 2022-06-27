ecf103d2f2171ddd946f228ad478fbdda1f4a30d
dahye1013 <alrean@naver.com>
2022-06-27 14:30:06 +0900

    ✨ : feature feedback form submit

--- 38-feedback/index.js
+++ 38-feedback/index.js
@@ -1,1 +1,40 @@
-console.log('This is boilerplate');
+/**
+[Event Bubbling] 
+- to not have to add an event listner on each element.
+- take advantage of event bubbling and event propagation.
+ */
+
+const $ratings = document.querySelectorAll('.rating')
+const $sendButton = document.querySelector('#send')
+const $panel = document.querySelector('#panel')
+
+let selectedRating = 'Satisfied'
+
+const setEvent = () => {
+    $panel.addEventListener('click', (e) => {
+        if (e.target.parentNode.classList.contains('rating')) {
+            removeActive()
+            e.target.parentNode.classList.add('active')
+            selectedRating = e.target.nextElementSibling.innerHTML
+            console.log(selectedRating);
+        }
+    })
+
+    $sendButton.addEventListener('click', () => {
+        $panel.innerHTML = /* html */ `
+			<i class="fas fa-heart"></i>
+			<strong>Thank you!</strong>
+			<br/>
+			<strong>Feedback: ${selectedRating}</strong>
+			<p>We'll use your feedback to improve our service.</p>
+			`
+    })
+
+    const removeActive = () => {
+        $ratings.forEach(($ratings) => $ratings.classList.remove('active'))
+    }
+
+}
+
+
+setEvent()
