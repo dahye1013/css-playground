aa6bf4bda3c58795f3b0b7e4b9607c6995c4a443
dahye1013 <alrean@naver.com>
2022-09-04 17:24:13 +0900

    ✨: generate password

--- 44-password-generator/index.js
+++ 44-password-generator/index.js
@@ -1,1 +1,48 @@
+import {
+  resultElement,
+  lengthElement,
+  upperCaseElement,
+  lowerCaseElement,
+  numberCaseElement,
+  symbolsCaseElement,
+  generateBtn,
+} from './selector.js';
+
 import randomFunc from './util/randomFunc.js';
+
+const generatePassword = (length, types) => {
+  let generatePassword = Array.from({ length });
+  const selectedTypes = Object.entries(types)
+    .filter(([_, v]) => v)
+    .map(([k]) => k);
+
+  for (let i = 0; i < generatePassword.length; i += selectedTypes.length) {
+    selectedTypes.forEach((typeKey, j) => {
+      generatePassword[i + j] = randomFunc[typeKey]();
+    });
+  }
+
+  return generatePassword.slice(0, length).join('');
+};
+
+generateBtn.addEventListener('click', () => {
+  const length = lengthElement.valueAsNumber;
+  const isUpper = upperCaseElement.checked;
+  const isLower = lowerCaseElement.checked;
+  const isNumber = numberCaseElement.checked;
+  const isSymbol = symbolsCaseElement.checked;
+
+  if (!length) {
+    alert('please enter the password length.');
+    return;
+  }
+
+  const types = {
+    lower: isUpper,
+    upper: isLower,
+    number: isNumber,
+    symbol: isSymbol,
+  };
+
+  resultElement.innerText = generatePassword(length, types);
+});
---
+++ 44-password-generator/selector.js
@@ -0,0 +1,8 @@
+export const resultElement = document.getElementById('result');
+export const lengthElement = document.getElementById('length');
+export const upperCaseElement = document.getElementById('uppercase');
+export const lowerCaseElement = document.getElementById('lowercase');
+export const numberCaseElement = document.getElementById('numbers');
+export const symbolsCaseElement = document.getElementById('symbols');
+export const generateBtn = document.getElementById('generate');
+export const clipboardEl = document.getElementById('clipboard');
