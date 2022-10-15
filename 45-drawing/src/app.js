4634b4f4fe16168c6bad9d11f33bf09ab7e7260a
dahye1013 <alrean@naver.com>
2022-10-15 20:54:26 +0900

    ✨: update color, size and clear pad

--- 45-drawing/src/app.js
+++ 45-drawing/src/app.js
@@ -6,7 +6,8 @@ const canvasState = {
   isPress: false,
   x: undefined,
   y: undefined,
-  color: 'black',
+  color: '#000',
+  size: 10,
   context: canvas.getContext('2d'),
   mousedown: e => {
     canvasState.isPress = true;
@@ -22,8 +23,28 @@ const canvasState = {
     if (canvasState.isPress) {
       const x = e.offsetX;
       const y = e.offsetY;
-      drawCircle(ctx, { x, y });
+      drawCircle(ctx, { x, y }, { size: canvasState.size, color: canvasState.color });
     }
+  },
+  updateColor: e => {
+    canvasState.color = e.target.value;
+  },
+  increaseSize: e => {
+    canvasState.size += 5;
+    if (canvasState.size > 50) {
+      canvasState.size = 50;
+    }
+    sizeEl.textContent = canvasState.size;
+  },
+  decreaseSize: e => {
+    canvasState.size -= 5;
+    if (canvasState.size <= 0) {
+      canvasState.size = 5;
+    }
+    sizeEl.textContent = canvasState.size;
+  },
+  clearPad: () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
   },
 };
 
@@ -31,6 +52,10 @@ const App = () => {
   canvas.addEventListener('mousedown', e => canvasState.mousedown(e));
   canvas.addEventListener('mouseup', canvasState.mouseup);
   canvas.addEventListener('mousemove', e => canvasState.mousemove(e));
+  colorEl.addEventListener('change', e => canvasState.updateColor(e));
+  increaseBtn.addEventListener('click', e => canvasState.increaseSize(e));
+  decreaseBtn.addEventListener('click', e => canvasState.decreaseSize(e));
+  clearEl.addEventListener('click', canvasState.clearPad);
 };
 
 App();
